81c3c8c3d9f565102943cf96c411f09f46824fbb
Bence Kovács <23636204+githappens@users.noreply.github.com>
2025-09-12 13:25:36 +0200

    Refactor DropdownComponent to inherit from juce::ComboBox and remove unused methods

--- src/DropdownComponent.h
+++ src/DropdownComponent.h
@@ -1,45 +1,21 @@
 #pragma once
 
-class DropdownComponent : public juce::Component
+class DropdownComponent : public juce::ComboBox
 {
 public:
-    struct DropdownItem
-    {
-        juce::String text;
-        int id;
-    };
-
     DropdownComponent (const juce::String& name, const char* binaryData, int binaryDataSize);
     ~DropdownComponent() override;
 
-    void addItem (const juce::String& itemText, int itemId);
-    void setSelectedId (int itemId, juce::NotificationType notification = juce::sendNotification);
-    int getSelectedId() const;
-    const juce::String& getSelectedText() const;
-
-    const std::vector<DropdownItem>& getItems() const { return items; }
-
-    std::function<void (int)> onSelectionChanged;
-
 protected:
-
     virtual void paintDropdownContent (juce::Graphics& g) {}
-    virtual juce::PopupMenu::Options configurePopupOptions (juce::PopupMenu::Options options);
-    virtual void customizePopupMenu (juce::PopupMenu& menu) {}
-
     const juce::String& getComponentName() const { return componentName; }
 
 private:
     void paintOverChildren (juce::Graphics& g) override;
     void resized() override;
-    void mouseDown (const juce::MouseEvent& event) override;
-    void showPopupMenu();
 
     StaticImageComponent backgroundImage;
-    std::vector<DropdownItem> items;
-    int selectedId = -1;
     juce::String componentName;
-    juce::String selectedText;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DropdownComponent)
 };
